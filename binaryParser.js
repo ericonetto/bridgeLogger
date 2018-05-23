11800ddfdda074923ce90be21ca3348df3389a15
Erico <ericonetto@inmetrics.com.br>
2018-05-22 23:05:10 -0300

    protocol

--- binaryParser.js
+++ binaryParser.js
@@ -1,15 +1,11 @@
 "use strict";
 var binaryParserClass = class bparser  {  
-  constructor(bitmapJson, data, start=0, end=0) {
-    if(end<=0){
-      end=data.length;
-    }
+  constructor(bitmapJson, data, start=0) {
     if(start<0){
       start=0;
     }
     
     this.bitmapJson=bitmapJson;
-    this.data=data.slice(start,end)
 
     var startPos=start;
     for (var key in this.bitmapJson) {
