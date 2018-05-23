5a5503718c92507f606ca90b63783dea7306f2b1
Erico <ericonetto@inmetrics.com.br>
2018-05-23 01:46:16 -0300

    protocol

--- parseTests.js
+++ parseTests.js
@@ -78,38 +78,55 @@ bridgeServer.on("targuetData",(data)=>{
               lac: 2,
               cid: 4,
               rxlev: 1
+            },
+            bit2:{
+              lac: 2,
+              cid: 4,
+              rxlev: 1
+            },
+            bit4:{
+              lac: 2,
+              cid: 4
             }
           }
         },
-        status:2,
-        battery:2,
-        ain0:2,
-        ain1:2,
-        mileage:4,
-        gsmcntr:2,
-        gpscntr:2,
-        pdmstep:2,
-        pdmtime:2,
-        temperature:2,
-        humidity:2,
-        illuminance:4,
-        co2:4
+        extra:{
+          status:2,
+          battery:2,
+          ain0:2,
+          ain1:2,
+          mileage:4,
+          gsmcntr:2,
+          gpscntr:2,
+          pdmstep:2,
+          pdmtime:2,
+          temperature:2,
+          humidity:2,
+          illuminance:4,
+          co2:4
+        }
       }
     }
   }
+
+
+  //test
+  //data=new Buffer("676712004300265b049d7403fd788323fafda8e50309000000000002d4000355fb00000eb43a007b16d505dd000000000000758e758802c40199000000000000000000000000011a",'hex');
+  
 
   var startPosition=0;
   while(startPosition<data.length){
     var msgFormat= new binaryParser(msgmap.format,data,startPosition);
 
     var packetSize=parseInt(msgFormat.size.toString('hex'),16);
-    console.log("mark:" + msgFormat.mark.toString('hex'));
-    console.log("pid:" +  msgFormat.pid.toString('hex'));
-    console.log("size:" + packetSize);
-    console.log("sequence:" + msgFormat.sequence.toString('hex'));
+    console.log("mark: " + msgFormat.mark.toString('hex'));
+    console.log("pid: " +  msgFormat.pid.toString('hex'));
+    console.log("size: " + packetSize);
+    console.log("sequence: " + msgFormat.sequence.toString('hex'));
   
     var pid=msgFormat.pid.toString('hex');
     var pidMap=msgmap.content[pid];
+
 
     console.log("msgFormat.lastPosition: " + msgFormat.lastPosition);
     
@@ -123,21 +140,43 @@ bridgeServer.on("targuetData",(data)=>{
     }else if(pid=="12"){
       var locationFormatMap=msgmap.content["12"].location.format;
       var locationHead= new binaryParser(locationFormatMap,data, msgFormat.lastPosition);
-      console.log("locationHead.time:" +  locationHead.time.toString('hex'));
-      console.log("locationHead.mask:" +  locationHead.mask.toString('hex'));
+      console.log("locationHead.time: " +  locationHead.time.toString('hex'));
+      console.log("locationHead.mask: " +  locationHead.mask.toString('hex'));
 
-      
-      if(locationHead.mask.toString(2)[0]=="1"){
+      var mask=(parseInt(locationHead.mask[0], 16)).toString(2);
+      if(mask[0]=="1"){
         var locationDataMap=msgmap.content["12"].location.masks.bit0;
         var locationData= new binaryParser(locationDataMap, data,locationHead.lastPosition);
-        console.log("locationData.latitude:" +  locationData.latitude.toString('hex'));
-        console.log("locationData.longitude:" +  locationData.longitude.toString('hex'));
-        console.log("locationData.altitude:" +  locationData.altitude.toString('hex'));
-        console.log("locationData.speed:" +  locationData.speed.toString('hex'));
-        console.log("locationData.course:" +  locationData.course.toString('hex'));
-        console.log("locationData.satellites:" +  locationData.satellites.toString('hex'));
+        for (var key in locationDataMap) {
+          console.log("locationData." + key + ": " + locationData[key].toString('hex'));
+        }
       }
-      
+      if(mask[1]=="1"){
+        var locationDataMap=msgmap.content["12"].location.masks.bit1;
+        var locationData= new binaryParser(locationDataMap, data,locationData.lastPosition);
+        for (var key in locationDataMap) {
+          console.log("locationData." + key + ": " + locationData[key].toString('hex'));
+        }
+      }
+      if(mask[2]=="1" || mask[3]=="1"){
+        var locationDataMap=msgmap.content["12"].location.masks.bit2;
+        var locationData= new binaryParser(locationDataMap, data,locationData.lastPosition);
+        for (var key in locationDataMap) {
+          console.log("locationData." + key + ": " + locationData[key].toString('hex'));
+        }
+      }
+      if(mask[4]=="1" || mask[5]=="1" || mask[6]=="1"){
+        var locationDataMap=msgmap.content["12"].location.masks.bit4;
+        var locationData= new binaryParser(locationDataMap, data,locationData.lastPosition);
+        for (var key in locationDataMap) {
+          console.log("locationData." + key + ": " + locationData[key].toString('hex'));
+        }
+      }
+      var locationDataMap=msgmap.content["12"].extra;
+      var locationData= new binaryParser(locationDataMap, data,locationData.lastPosition);
+      for (var key in locationDataMap) {
+        console.log("locationData." + key + ": " + locationData[key].toString('hex'));
+      }
 
     }else{
       lastPosition=data.length;
